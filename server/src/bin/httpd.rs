14759295742557e3dbd68bdb602c85ebd7ba5602
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-12-16 22:51:16 -0800

    chore: add back dependencies to run on Shuttle

--- server/src/bin/httpd.rs
+++ server/src/bin/httpd.rs
@@ -9,26 +9,26 @@ use tower::{make::Shared, service_fn, ServiceBuilder, ServiceExt};
 use tower_http::normalize_path::NormalizePathLayer;
 
 // function to Shuttleize the main service
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[shuttle_runtime::main]
+async fn main() -> shuttle_tower::ShuttleTower<Router> {
     let mut router = Router::default();
 
     router.on(Method::GET, "/", service_fn(svc_root).boxed_clone());
     router.on(Method::GET, "/-1/error", service_fn(svc_fake_error).boxed_clone());
 
-    let hyper_service = ServiceBuilder::new()
+    ServiceBuilder::new()
         .layer(NormalizePathLayer::trim_trailing_slash())
         .service(router);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    let make_service = Shared::new(hyper_service);
+    // let make_service = Shared::new(hyper_service);
 
-    let server = Server::bind(&addr).serve(make_service);
+    // let server = Server::bind(&addr).serve(make_service);
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    // if let Err(e) = server.await {
+    //    eprintln!("server error: {}", e);
+    // }
 
-    Ok(())
+    Ok(router.into())
 }
